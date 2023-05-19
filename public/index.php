3d4e4ea7d456e0c63683ced8bc1e65e220c4c0b8
iballi2017 <iballi2017@gmail.com>
2023-05-19 01:38:35 +0100

    landing page

--- public/index.php
+++ public/index.php
@@ -7,11 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HAC | </title>
     <link rel="stylesheet" href="./assets/stylesheets/main.css">
+    <!-- <link rel="stylesheet" href="./assets/stylesheets/bootstrap-5.2.3/bootstrap.min.css"> -->
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/490087c906.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="main-wrapper">
-        <header>
+    <!-- <div class="main-wrapper"> -->
+    <header>
+        <!-- <div class="sticky-wrap"> -->
             <div class="top-bar fs-50 base-text-color-100 bg-secondary-500">
                 <span>9, Tinubu Street, off Coker road, illupeju Lagos.</span>
                 <span>clientservices@hacmedical.org</span>
@@ -20,11 +24,16 @@
             <div class="primary-header">
                 <div class="container">
                     <!-- TOOLBAR -->
-                    <div class="inner">
+                    <div class="nav-wrapper">
                         <a href="#" class="brand-logo">
                             <img src="./assets/images/brand-logo.svg" alt="Mainland Clinics" class="img-fluid">
                         </a>
-                        <nav class="primary-navigation">
+                        <button class="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false">
+                            <i class="fas fa-bars"></i>
+                            <i class="fas fa-times"></i>
+                            <span class="visually-hidden">Menu</span>
+                        </button>
+                        <nav class="primary-navigation" id="primary-navigation">
                             <ul role="list" class="nav-list" aria-label="primary">
                                 <li><a href="#">About us</a></li>
                                 <li><a href="#">Services</a></li>
@@ -35,84 +44,90 @@
                             <li><a href="#">Shopping Cart</a></li> -->
                             </ul>
                         </nav>
-                        <button>Book an appointment</button>
-                        <a href="#">Shopping Cart</a></li>
+                        <button class="button cta | display-md-inline display-sm-none">Book an appointment</button>
+                        <a href="#" class="display-md-inline display-sm-none">Shopping Cart</a>
                     </div>
                 </div>
             </div>
-        </header>
+        <!-- </div> -->
+    </header>
 
-        <main>
-            <!-- Hero slider -->
-            <div class="hero-slider"></div>
+    <main>
+        <!-- Hero slider -->
+        <div class="hero-slider"></div>
 
 
-            <section>
-                <div class="container">
-                    <div class="even-columns">
-                        <div class="border-radius-500 overflow-hidden" style="margin-right: 60px">
-                            <img src="./assets/images/featured-image-1.png" alt="Take control of your health" class="img-fluid">
-                        </div>
-                        <div>
-                            <h2 class="fs-section-title">About us</h2>
-                            <h3 class="fs-section-headline fw-bold">Take Control of Your Health Today</h3>
-                            <p>At Mainland Clinic, we are dedicated to providing the highest quality of medical care and wellness support to our patients. We believe that everyone deserves access to compassionate, personalized healthcare that meets their unique needs and goals.</p>
-                            <a href="#" class="cta | text-decoration-none">Read more</a>
-                        </div>
+        <section class="padding-block-800">
+            <div class="container">
+                <div class="even-columns">
+                    <div class="overflow-hidden">
+                        <img src="./assets/images/featured-image-1.png" alt="Take control of your health" class="img-fluid">
+                    </div>
+                    <div>
+                        <h2 class="fs-section-title">About us</h2>
+                        <h3 class="fs-section-headline fw-bold">Take Control of Your Health Today</h3>
+                        <p>At Mainland Clinic, we are dedicated to providing the highest quality of medical care and wellness support to our patients. We believe that everyone deserves access to compassionate, personalized healthcare that meets their unique needs and goals.</p>
+                        <a href="#" class="button">Read more</a>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section>
-                <div class="container services-list-grid">
-                    <div class="grid-row-1">
-                        <div>
-                            <h2 class="fs-section-title">Our services</h2>
-                            <h3 class="fs-section-headline fw-bold">Invest in Your Well -Being.</h3>
-                        </div>
-
-                        <ul role="list" class="services-list" aria-label="services list">
-                            <li class="service">
-                                <a href="#">
-                                    <div class="card-title">Clinical Service</div>
-                                </a>
-                            </li>
-                            <li class="service">
-                                <a href="#">
-                                    <div class="card-title">Food Handlers Test</div>
-                                </a>
-                            </li>
-                            <li class="service">
-                                <a href="#">
-                                    <div class="card-title">Pre-employment Test</div>
-                                </a>
-                            </li>
-                        </ul>
+        <section>
+            <div class="container services-list-grid">
+                <div class="grid-row-1">
+                    <div>
+                        <h2 class="fs-section-title">Our services</h2>
+                        <h3 class="fs-section-headline fw-bold">Invest in Your Well -Being.</h3>
                     </div>
 
-                    <div class="grid-row-2">
-                        <ul role="list" class="services-list" aria-label="continued services list">
-                            <li class="service">
-                                <a href="#">
-                                    <div class="card-title">Fitness/Wellness Test</div>
-                                </a>
-                            </li>
-                            <li class="service">
-                                <a href="#">
-                                    <div class="card-title">Telemedicine</div>
-                                </a>
-                            </li>
-                        </ul>
-                        <div>
-                            <h3>Choose Health. Choose Us</h3>
-                            <p>We Care for Your Health and Well-Being</p>
-                            <a href="#" class="cta | text-decoration-none">Shop now</a>
-                        </div>
+                    <ul role="list" class="services-list" aria-label="services list">
+                        <li class="service">
+                            <a href="#">
+                                <div class="card-title">Clinical Service</div>
+                            </a>
+                        </li>
+                        <li class="service">
+                            <a href="#">
+                                <div class="card-title">Food Handlers Test</div>
+                            </a>
+                        </li>
+                        <li class="service">
+                            <a href="#">
+                                <div class="card-title">Pre-employment Test</div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="grid-row-2">
+                    <ul role="list" class="services-list" aria-label="continued services list">
+                        <li class="service">
+                            <a href="#">
+                                <div class="card-title">Fitness/Wellness Test</div>
+                            </a>
+                        </li>
+                        <li class="service">
+                            <a href="#">
+                                <div class="card-title">Telemedicine</div>
+                            </a>
+                        </li>
+                    </ul>
+                    <div>
+                        <h3>Choose Health. Choose Us</h3>
+                        <p>We Care for Your Health and Well-Being</p>
+                        <a href="#" class="button">Shop now</a>
                     </div>
                 </div>
-            </section>
-        </main>
-    </div>
+            </div>
+        </section>
+    </main>
+    <!-- </div> -->
+
+
+    <!-- Custom script -->
+    <script src="./assets/scripts/index.js"></script>
 </body>
 
 </html>
+<!-- . -->
