6a4a41abe43457967a9b9109a3f2dbfd98e27a11
iballi2017 <iballi201@gmail.com>
2023-05-22 11:34:04 +0100

    user account

---
+++ public/views/account-information.php
@@ -0,0 +1,40 @@
+<!-- Header -->
+<?php include_once "../components/header.php" ?>
+
+<!-- Title section -->
+<div class="title-section-container">
+    <section class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="../" class="base-text-color-500 text-decoration-none">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Account Information</li>
+            </ol>
+        </nav>
+        <h1 class="fw-bold fs-500">Account Information</h1>
+    </section>
+</div>
+
+
+<section class="padding-block-800">
+    <div class="container">
+        <div class="border-accent-50 border rounded-250 p-3">
+            <div class="row g-3">
+                <div class="col-12 col-lg-4">
+                    <nav class="border-accent-50 border rounded-250 p-3">
+                        <ul role="list" class="ps-0 mb-0"  aria-label="side navigation">
+                            <li>fff</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-12 col-lg-8">
+                    <div class="border-accent-50 border rounded-250 p-3">main</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Footer -->
+<?php include_once "../components/footer.php" ?>
