5d235076b831e33554341e4342b46a29c900f772
iballi2017 <iballi2017@gmail.com>
2023-05-30 00:53:51 +0100

    implement design for shop page

---
+++ public/views/shop.php
@@ -0,0 +1,186 @@
+<!-- Header -->
+<?php include_once "../components/header.php" ?>
+
+<!-- Title section -->
+<section class="title-section-container">
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="../" class="base-text-color-500 text-decoration-none">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Shop</li>
+            </ol>
+        </nav>
+        <h1 class="fw-bold fs-500">Shop</h1>
+    </div>
+</section>
+
+
+<section class="">
+    <div class="container">
+
+        <div class="d-flex align-items-center justify-content-between mt-4">
+
+            <!-- filter select -->
+            <div class="custom-select rounded-250 mb-5" style="max-width: 200px; height: 36px;">
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Sort by</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                    <option value="3">Lorem ipsum dolor sit amet.</option>
+                </select>
+            </div>
+
+            <!--  -->
+            <div class="custom-input-group rounded-250 mb-5" style="max-width: 200px; height: 36px;">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+                    <button class="btn" type="button" id="button-addon2">
+                        <img src="../assets/images/icons/search-green.svg" alt="" class="img-fluid">
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
+        <h2 class="visually-hidden">Product listing</h2>
+        <div class="row g-3 mb-3">
+            <div class="col-6 col-md-3">
+                <a href="#" class="product-item-wrap">
+                    <div class="card rounded-250 border-accent-50">
+                        <div class="img-wrap">
+                            <img src="../assets/images/products/product-sample-1.png" class="card-img-top" alt="...">
+                        </div>
+
+                        <div class="card-body">
+                            <p class="fs-150">Accu chek Glucometer strip
+                                <br />
+                                <span class="fs-300 fw-semi-bold">₦2,400.00</span>
+                            </p>
+
+                        </div>
+
+                        <div class="card-footer | bg-white">
+                            <button class="btn add-to-cart | w-100">Add to cart</button>
+                            <span class="my-1 d-block"></span>
+                            <button class="btn add-to-wishlist | w-100">Add to wishlist</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product-item-wrap">
+                    <div class="card rounded-250 border-accent-50">
+                        <div class="img-wrap">
+                            <img src="../assets/images/products/product-sample-1.png" class="card-img-top" alt="...">
+                        </div>
+
+                        <div class="card-body">
+                            <p class="fs-150">Accu chek Glucometer strip
+                                <br />
+                                <span class="fs-300 fw-semi-bold">₦2,400.00</span>
+                            </p>
+
+                        </div>
+
+                        <div class="card-footer | bg-white">
+                            <button class="btn add-to-cart | w-100">Add to cart</button>
+                            <span class="my-1 d-block"></span>
+                            <button class="btn add-to-wishlist | w-100">Add to wishlist</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product-item-wrap">
+                    <div class="card rounded-250 border-accent-50">
+                        <div class="img-wrap">
+                            <img src="../assets/images/products/product-sample-1.png" class="card-img-top" alt="...">
+                        </div>
+
+                        <div class="card-body">
+                            <p class="fs-150">Accu chek Glucometer strip
+                                <br />
+                                <span class="fs-300 fw-semi-bold">₦2,400.00</span>
+                            </p>
+
+                        </div>
+
+                        <div class="card-footer | bg-white">
+                            <button class="btn add-to-cart | w-100">Add to cart</button>
+                            <span class="my-1 d-block"></span>
+                            <button class="btn add-to-wishlist | w-100">Add to wishlist</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product-item-wrap">
+                    <div class="card rounded-250 border-accent-50">
+                        <div class="img-wrap">
+                            <img src="../assets/images/products/product-sample-1.png" class="card-img-top" alt="...">
+                        </div>
+
+                        <div class="card-body">
+                            <p class="fs-150">Accu chek Glucometer strip
+                                <br />
+                                <span class="fs-300 fw-semi-bold">₦2,400.00</span>
+                            </p>
+
+                        </div>
+
+                        <div class="card-footer | bg-white">
+                            <button class="btn add-to-cart | w-100">Add to cart</button>
+                            <span class="my-1 d-block"></span>
+                            <button class="btn add-to-wishlist | w-100">Add to wishlist</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product-item-wrap">
+                    <div class="card rounded-250 border-accent-50">
+                        <div class="img-wrap">
+                            <img src="../assets/images/products/product-sample-1.png" class="card-img-top" alt="...">
+                        </div>
+
+                        <div class="card-body">
+                            <p class="fs-150">Accu chek Glucometer strip
+                                <br />
+                                <span class="fs-300 fw-semi-bold">₦2,400.00</span>
+                            </p>
+
+                        </div>
+
+                        <div class="card-footer | bg-white">
+                            <button class="btn add-to-cart | w-100">Add to cart</button>
+                            <span class="my-1 d-block"></span>
+                            <button class="btn add-to-wishlist | w-100">Add to wishlist</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+        <!-- Pagination -->
+        <!-- <div class="text-end"> -->
+        <nav aria-label="Page navigation" role="list">
+            <ul class="pagination justify-content-end">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
+        <!-- </div> -->
+    </div>
+</section>
+
+
+
+
+
+<!-- Footer -->
+<?php include_once "../components/footer.php" ?>
